c4b4e97d4d652e84083dd880f10f4058afb0d278
achuyanov <55102485+achuyanov@users.noreply.github.com>
2021-06-14 23:53:46 +0300

    task 5 1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,16 @@
 import Foundation
 
-class StockMaximize {
 
+class StockMaximize {
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var ret = 0
+        var temp = 0
+        
+        for i in 0..<prices.count {
+            temp = 0
+            for j in (i + 1)..<prices.count { temp = ((prices[j] - prices[i]) > temp) ? prices[j] - prices[i]: temp }
+            ret += temp
+        }
+        return ret
     }
 }
